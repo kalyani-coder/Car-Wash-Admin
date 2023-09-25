162ecdfe78d42c12096d5f5df6a52afe46037608
Vedant0370 <vedant.0370@gmail.com>
2023-09-25 15:48:33 +0530

    get by client with status and contact

--- backend2/src/models/BookingModel.js
+++ backend2/src/models/BookingModel.js
@@ -6,6 +6,7 @@ const bookingSchema = new mongoose.Schema({
 
     clientId: String,
     clientName: String,
+    clientContact : String,
     servicesName: String,
     date: String,
     time: String,
