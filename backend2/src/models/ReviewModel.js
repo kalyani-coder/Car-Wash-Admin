c583105af117cae1388d617680a5eb7088326c01
Vedant0370 <vedant.0370@gmail.com>
2023-09-29 16:05:20 +0530

    frontend new

--- backend2/src/models/ReviewModel.js
+++ backend2/src/models/ReviewModel.js
@@ -5,6 +5,7 @@ const reviewSchema = new mongoose.Schema({
     message : String,
     clientId : String,
     rating : String,
+    clientName :String,
 }) 
 
 const  newReviewSchema = mongoose.model('reviews' , reviewSchema);
