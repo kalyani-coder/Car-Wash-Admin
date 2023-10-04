06b9c76fb35b9897e45f6d316c3de5f46258cde1
Vedant0370 <vedant.0370@gmail.com>
2023-10-04 22:25:04 +0530

    get by booking id

--- backend2/src/routes/AgentsLocation.js
+++ backend2/src/routes/AgentsLocation.js
@@ -1,37 +1,36 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const AgentLocation = require("../models/AgentLocation"); // Adjust the path as needed
+const AgentLocation = require('../models/AgentLocation');
 
-// Page: Get all clients
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const clients = await AgentLocation.find();
     res.json(clients);
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-// router.get("/:field/:value", async (req, res) => {
-//   const field = req.params.field;
-//   const value = req.params.value.replace("_", " ");
-//   if (value && field) {
-//     try {
-//       let bookings;
-//       const schemaType = AgentLocation.schema.path(field);
-//       if (schemaType instanceof mongoose.Schema.Types.Array) {
-//         bookings = await AgentLocation.find({ [field]: { $in: [value] } });
-//       } else {
-//         bookings = await AgentLocation.find({ [field]: value });
-//       }
-//       res.json(bookings);
-//     } catch (e) {
-//       res.status(400).json({ message: "Bad request" });
-//     }
-//   } else {
-//     res.status(400).json({ message: "Bad request" });
-//   }
-// });
+router.get('/bookingID/:bookingID', async (req, res) => {
+  try {
+    const { bookingID } = req.params;
+
+    if (!bookingID) {
+      return res.status(400).json({ message: 'Invalid bookingID' });
+    }
+
+    const agentLocation = await AgentLocation.findOne({ bookingID });
+
+    if (!agentLocation) {
+      return res.status(404).json({ message: 'Agent location not found' });
+    }
+
+    res.json(agentLocation);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 router.get("/:field/:value", async (req, res) => {
   const field = req.params.field;
@@ -54,34 +53,17 @@ router.get("/:field/:value", async (req, res) => {
   }
 });
 
-// router.get("/:field/:value", async (req, res) => {
-//   const field = req.params.field;
-//   const value = req.params.value.replace("_", " ");
-//   if (value && field) {
-//     try {
-//       const bookings = await AgentLocation.find({ [field]: value });
-//       res.json(bookings);
-//     } catch (e) {
-//       res.status(400).json({ message: "Bad request" });
-//     }
-//   } else {
-//     res.status(400).json({ message: "Bad request" });
-//   }
-// });
-
-// Page: Create a new client
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const newAgentLocation = new AgentLocation(req.body);
     await newAgentLocation.save();
     res.status(201).json(newAgentLocation);
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-// Page: Update a client by ID
-router.patch("/:id", async (req, res) => {
+router.patch('/:id', async (req, res) => {
   const clientId = req.params.id;
   try {
     const updatedAgentLocation = await AgentLocation.findByIdAndUpdate(
@@ -93,12 +75,11 @@ router.patch("/:id", async (req, res) => {
     );
     res.json(updatedAgentLocation);
   } catch (error) {
-    res.status(404).json({ message: "AgentLocation not found" });
+    res.status(404).json({ message: 'AgentLocation not found' });
   }
 });
 
-// Page: Delete a client by ID
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const clientId = req.params.id;
   try {
     const deletedAgentLocation = await AgentLocation.findByIdAndRemove(
@@ -106,24 +87,22 @@ router.delete("/:id", async (req, res) => {
     );
     res.json(deletedAgentLocation);
   } catch (error) {
-    res.status(404).json({ message: "AgentLocation not found" });
+    res.status(404).json({ message: 'AgentLocation not found' });
   }
 });
 
-// get by id
-
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const clientId = req.params.id;
 
   try {
     const newAgentLocation = await AgentLocation.findById(clientId);
     if (!newAgentLocation) {
-      return res.status(404).json({ message: "AgentLocation not Found" });
+      return res.status(404).json({ message: 'AgentLocation not Found' });
     }
     res.json(newAgentLocation);
   } catch (e) {
     res.status(404).json({
-      message: "AgentLocation by id not found Internal server error",
+      message: 'AgentLocation by id not found Internal server error',
     });
   }
 });
